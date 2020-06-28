cd424bf3900ba851ffda8c34e43a7d6606912cb1
Ilja B <49001925+iljucha@users.noreply.github.com>
2020-06-28 13:59:34 +0200

    Create secure.js

---
+++ plugins/secure.js
@@ -0,0 +1,22 @@
+export default function secure() {
+    return (ctx, next) => {
+        ctx.header = {
+            "X-XSS-Protection": "1; mode=block",
+            "X-Content-Type-Options": "nosniff",
+            "X-Frame-Options": "DENY",
+            "X-Download-Options": "noopen",
+            "X-Robots-Tag": "noarchive",
+            "Referrer-Policy": "no-referrer",
+            "Content-Security-Policy": 
+                "default-src 'none';" +
+                "script-src 'self';" +
+                "connect-src 'self';" +
+                "img-src 'self' data:;" +
+                "style-src 'self' 'unsafe-inline';" +
+                "font-src 'self';",
+            "Cache-Control": "no-store, no-cache, must-revalidate",
+            "pragma": "no-cache"
+        }
+        next()
+    }
+}
